2ed2586b50e124ed9f10fd8705f07b694fc6eae9
edward izquierdo <61303005+edward4212@users.noreply.github.com>
2020-09-25 19:34:05 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,10 +4,10 @@ var usuario=document.login.usuario.value;
 var password=document.login.password.value; 
 
 if (usuario=="Admi" && password=="Admi") { 
-    window.location="https://sites.google.com/view/calidadfonedh/p%C3%A1gina-principal?authuser=1"; 
+    window.location="https://sites.google.com/view/calidadfonedh/calidad?authuser=1"; 
 } else{
-    if (usuario=="asesorcalidad" && password=="Dralei89") { 
-        window.location="https://sites.google.com/view/calidadfonedh/p%C3%A1gina-principal?authuser=1"; 
+    if (usuario=="Gerente" && password=="hmekRJJv") { 
+        window.location="https://sites.google.com/view/calidadfonedh/calidad?authuser=1"; 
 } else{
     if (usuario=="" && password=="") { 
         window.location="errorpopup.html"; 
